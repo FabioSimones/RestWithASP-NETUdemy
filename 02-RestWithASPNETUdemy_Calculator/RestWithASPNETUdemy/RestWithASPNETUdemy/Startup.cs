5915b251a94179ed9aa2552719f1b9b42a942db2
Fábio simones <simones.fabio1994@gmail.com>
2024-07-21 09:55:20 -0300

    Create table person and connection with visual studio.

--- 02-RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ 02-RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -1,17 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Model.Context;
 using RestWithASPNETUdemy.Services;
 using RestWithASPNETUdemy.Services.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RestWithASPNETUdemy
 {
@@ -28,6 +24,12 @@ namespace RestWithASPNETUdemy
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            var mySqlConnection = Configuration.GetConnectionString("MySqlConnectionString");
+
+            services.AddDbContext<MySqlContext>(options =>
+                              options.UseMySql(mySqlConnection,
+                                ServerVersion.AutoDetect(mySqlConnection)));
 
             //Dependency injection
             services.AddScoped<IPersonService, PersonService>();
